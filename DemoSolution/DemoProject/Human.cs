b10aa9e194c9275cde33c28c4c00fcc6b223a6c2
JP <jp@jp.nl>
2025-07-08 13:27:48 +0200

    Eerste projectje gemaakt met wat C#-code

---
+++ DemoSolution/DemoProject/Human.cs
@@ -0,0 +1,53 @@
+﻿
+namespace DemoProject
+{
+    public class Human
+    {
+        // testing
+        // mocking
+        public static int HumanCounter { get; set; }
+
+        public string Name { get; set; }
+
+
+        public Human() // constructor  new()
+        {
+            HumanCounter++;
+            Console.WriteLine($"new human! counter is nu {HumanCounter}");
+        }
+
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value > 150 || value < 0)
+                {
+                    throw new ArgumentException("No way die leeftijd");
+                }
+
+                Console.WriteLine("Netjes ingevuld ouwe!");
+                _age = value;
+            }
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine($"Hoi ik ben {Name}");
+        }
+    }
+
+    public enum Haarkleur
+    {
+        Blond,
+        Bruin,
+        Zwart,
+        Rood
+    }
+
+}
+
---
+++ DemoSolution/DemoProject/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DemoProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            //float f1 = 0.3f;
+            //float f2 = 0.2f;
+            //Console.WriteLine(f1 - f2);
+
+            //var jp = new Human();
+            //jp.Name = "Jan Peter";
+            //jp.Age = 38;
+
+            var jp = new Human
+            {
+                Name = "Jan Peter",
+                Age = 38
+            };
+            jp.Speak();
+
+            var elon = new Human
+            {
+                Name = "Elon Musk",
+                Age = 59
+            };
+            elon.Speak();
+
+            Console.WriteLine($"{jp.Name} is {jp.Age} years old.");
+            
+        }
+    }
+}
